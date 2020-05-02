0b4be54f29c8fdda13e1e9ab684c216a45c85b2c
Thiago <Thiago@DESKTOP-M5OB7E8>
2020-05-01 21:21:36 -0300

    - Criação da mecânica de iniciar o campeonato.

--- src/java/model/domain/Campeonato.java
+++ src/java/model/domain/Campeonato.java
@@ -7,8 +7,6 @@ package model.domain;
 
 import java.io.Serializable;
 import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.List;
 import java.util.List;
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
