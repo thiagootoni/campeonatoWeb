64f06f735d61f7aca9c2ce4acbaa708e7cbc7aed
hugo.alexandre <hugo.alexandre@WS169.Processa.com>
2020-05-07 15:51:57 -0300

    ajuste final artilharia

--- src/java/controller/action/view/CallViewArtilharia.java
+++ src/java/controller/action/view/CallViewArtilharia.java
@@ -15,6 +15,7 @@ import javax.servlet.http.HttpServletResponse;
 import model.DTO.CampeonatoPainelAdmDto;
 import model.dao.impl.CampeonatoDao;
 import model.dao.impl.JogadorDao;
+import model.domain.Campeonato;
 import model.domain.Gol;
 import model.domain.Jogador;
 
@@ -35,20 +36,17 @@ public class CallViewArtilharia implements ICommanderAction {
         RequestDispatcher rd = request.getRequestDispatcher("Template.jsp?page=painelArtilharia");
 
         //Pegando artilheiro do campeonato
-        try {
+        JogadorDao jDao = new JogadorDao();
 
-            Jogador artilheiro = new JogadorDao()
-                    .buscarUm(
-                            new CampeonatoDao()
-                                    .getCampeonatoAberto()
-                                    .getArtilheiro()
-                                    .getId());
+        Campeonato camps = new CampeonatoDao()
+                .getCampeonatoAberto();
+                
 
-            request.setAttribute("artilheiro", artilheiro);
-        } //int golsFeitos = artilheiro.getGolsFeitos().size();
-        catch (Exception ex) {
-            List<Jogador> todosJogadores = new JogadorDao().buscarTodos();
+        if (camps == null || camps.getArtilheiro()==null) {
 
+            List<Jogador> todosJogadores = jDao.buscarTodos();
+
+            System.out.println(todosJogadores.get(2).getGolsFeitos().size());
             List<Jogador> jogadoresGoleadores = new ArrayList<>();
             int i = 0;
             for (Jogador jogador : todosJogadores) {
@@ -70,7 +68,54 @@ public class CallViewArtilharia implements ICommanderAction {
 
             request.setAttribute("jogadoresGoleadores", jogadoresGoleadores);
             request.setAttribute("artilheiro", null);
+
+        } else {
+            Jogador artilheiro = jDao
+                    .buscarUm(camps.getId());
+
+            request.setAttribute("artilheiro", artilheiro);
         }
+/*
+        try {
+
+            Jogador artilheiro = jDao
+                    .buscarUm(
+                            new CampeonatoDao()
+                                    .getCampeonatoAberto()
+                                    .getArtilheiro()
+                                    .getId());
+
+            request.setAttribute("artilheiro", artilheiro);
+        } //int golsFeitos = artilheiro.getGolsFeitos().size();
+        catch (Exception ex) {
+            List<Jogador> todosJogadores = jDao.buscarTodos();
+
+            System.out.println(todosJogadores.get(2).getGolsFeitos().size());
+            List<Jogador> jogadoresGoleadores = new ArrayList<>();
+            int i = 0;
+            for (Jogador jogador : todosJogadores) {
+                jogador.getGolsFeitos();
+                for (Gol golsFeito : jogador.getGolsFeitos()) {
+                    golsFeito.getId();
+                }
+                if (jogador.getGolsFeitos().size() > 0) {
+
+                    jogadoresGoleadores.add(jogador);
+                }
+                i++;
+            }
+            Collections.sort(
+                    jogadoresGoleadores,
+                    (j1, j2)
+                    -> j2.getGolsFeitos().size()
+                    - j1.getGolsFeitos().size());
+
+            request.setAttribute("jogadoresGoleadores", jogadoresGoleadores);
+            request.setAttribute("artilheiro", null);
+        } finally {
+            jDao.close();
+            //System.out.println("Passei aq");
+        }*/
 
         rd.forward(request, response);
     }
